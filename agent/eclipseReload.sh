17c8006ffdf93ea0c9eeec94d72e05ea3391aa42
tealee1992 <tealee1992@163.com>
2017-11-07 20:36:59 +0800

    fix eclipseReload.sh

--- agent/eclipseReload.sh
+++ agent/eclipseReload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # this script is for processing stuff before stop container
 WID=`xdotool search --name "Eclipse Platform" | head -1`
-if [-z WID]; then
-	echo"eclipse is not running"
-else
+if [ -n "$WID" ]
+then
 	xdotool windowactivate $WID
 	xdotool windowfocus $WID
 	xdotool key ctrl+S
 	xdotool windowkill $WID
+else
+	echo"eclipse is not running"
 fi	
 
