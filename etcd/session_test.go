3a94de49a8906a536aef9f722971c2a432b3b32e
tealee1992 <tealee1992@163.com>
2017-12-19 15:23:42 +0800

    fix

--- etcd/session_test.go
+++ etcd/session_test.go
@@ -5,31 +5,32 @@ import (
 )
 
 func Test_Set(t *testing.T) {
-	conid = "000"
-	hostip = "11.0.0.172"
-	url = "11.0.0.172:9500"
+	conid := "0945a9b40e4e"
+	hostip := "11.0.0.172"
+	url := "11.0.0.172:9500"
 	// set lab session
-	labSession := etcd.Session{
+	labSession := Session{
 		IP:     hostip,
 		ConID:  conid,
 		Status: "started",
 		Url:    url,
 	}
+	userid := "000"
 	labSession.Set(userid)
 }
 
 func Test_GetPort(t *testing.T) {
-	conid = "000"
-	hostip = "11.0.0.172"
-	url = "11.0.0.172:9500"
+	conid := "0945a9b40e4e"
+	hostip := "11.0.0.172"
+	url := "11.0.0.172:9500"
 	// set lab session
-	labSession := etcd.Session{
+	labSession := Session{
 		IP:     hostip,
 		ConID:  conid,
 		Status: "started",
 		Url:    url,
 	}
+	userid := "000"
 	labSession.Set(userid)
-	port := labSession.getPort()
 	t.Log(port)
 }
